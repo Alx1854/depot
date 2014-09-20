9f441bb98254cac89a201213f5b047f559e3d883
Alx1854 <AlexC_3011@hotmail.com>
2014-09-20 12:14:54 -0500

    Validation & Test!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+	#The validates() method is the standard Rails validator.
+	#It will check one or more model fields against one or more conditions.
+	#presence: true tells the validator to check that each of the named
+	#fields is present and its contents are not empty.
+	validates :title, :description, :image_url, presence: true
+	#use the delightfully named numericality() option to verify that the price is a valid number.
+	#We also pass the rather verbosely named :greater_than_or_equal_to option a value of 0.01.
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	#to ensure that no other row in the products table has the same title as the row we’re about to save.
+	validates :title, uniqueness: true
+	#check that the URL ends with one of .gif, .jpg, or .png.
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be URL for GIF, JPG, or PNG image.'
+	}
 end
